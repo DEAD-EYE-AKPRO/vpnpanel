70bac5bf5154b593c4d8c5f9c8e0c766b6b4cf53
AK <85071009+DEAD-EYE-AKPRO@users.noreply.github.com>
2025-07-06 13:18:45 -0400

    Update install_vpnpanel.sh

--- install_vpnpanel.sh
+++ install_vpnpanel.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 set -e
-
+echo "=== AKAKAKAKAKAKAKAKAKAKAKAK ==="
 echo "=== VPN Panel Auto Installer ==="
+echo "=== AKAKAKAKAKAKAKAKAKAKAKAK ==="
 
 
 DOMAIN_NAME=(ip addr show $(ip route | awk '/default/ { print $5 }') | grep "inet " | awk '{print $2}' | cut -d'/' -f1)
 APP_DIR="/opt/vpnpanel"
 REPO_URL="https://github.com/DEAD-EYE-AKPRO/vpnpanel.git"
 FLASK_PORT="1227"
+chmod +x openvpn-install.sh
 
-# Install dependencies
 echo "[*] Installing system dependencies..."
 apt update
 apt install -y python3 python3-pip python3-venv nginx certbot python3-certbot-nginx sudo
